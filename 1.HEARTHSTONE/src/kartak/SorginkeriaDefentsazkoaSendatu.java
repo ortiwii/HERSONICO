4afdfcca55cd955641d6b3109146b5c7fe669365
Ortiwii <Ortiwii@192.168.0.16>
2019-04-25 12:00:03 +0200

    senadatu

--- 1.HEARTHSTONE/src/kartak/SorginkeriaDefentsazkoaSendatu.java
+++ 1.HEARTHSTONE/src/kartak/SorginkeriaDefentsazkoaSendatu.java
@@ -5,26 +5,28 @@ import a.projectPackage.Teklatua;
 
 public class SorginkeriaDefentsazkoaSendatu extends SorginkeriaDefentsazkoa 
 {
-	//atributoak
-	private int sendatuKop;
-
-	//eraikitzailea
-	public SorginkeriaDefentsazkoaSendatu(int pIdKarta, String pIzena, String pDeskribapena,int pBalioa, int pSendatuKop) 
-	{
-		super (pIdKarta, pIzena, pDeskribapena,pBalioa);
-		this.sendatuKop = pSendatuKop;
-	}
 	
-	//gainontzeko metodoak
-	public void jokatuKarta ()
-	{
-		Karta nireKarta = Teklatua.getNireTeklatua().irakurriAukera("Zure zelaiko karten artean, zeini eman nahi diziozu "+this.sendatuKop+" bizitza punto?", 
-				Partida.getNirePartida().getUnekoJokalaria().getNireZelaikoKartak());
-		this.emanDefentsa(nireKarta);
-	}
-	public void emanDefentsa (Karta pKarta)
-	{
-		((Morroia)pKarta).kartaHoniErasotu(this.sendatuKop*(-1));
-		pKarta.imprimatu();
-	}
+	//atributoak
+		private int sendatuKop;
+
+		//eraikitzailea
+		public SorginkeriaDefentsazkoaSendatu(int pIdKarta, String pIzena, String pDeskribapena,int pBalioa, int pSendatuKop) 
+		{
+			super (pIdKarta, pIzena, pDeskribapena,pBalioa);
+			this.sendatuKop = pSendatuKop;
+		}
+		
+		//gainontzeko metodoak
+		public void jokatuKarta ()
+		{
+			Karta nireKarta = Teklatua.getNireTeklatua().irakurriAukera("Zure zelaiko karten artean, zeini eman nahi diziozu "+this.sendatuKop+" bizitza punto?", 
+					Partida.getNirePartida().getUnekoJokalaria().getNireZelaikoKartak());
+			this.emanDefentsa(nireKarta);
+		}
+		public void emanDefentsa (Karta pKarta)
+		{
+			((Morroia)pKarta).kartaHoniErasotu(this.sendatuKop*(-1));
+			pKarta.imprimatu();
+		}
+
 }
