ee854345a9420e4aa2596a3e13aa45688c6bf9db
Martín Amézola <Martn Amzola@192.168.0.18>
2019-05-07 19:12:19 +0200

    Francisco franco did nothing wrong

--- 1.HEARTHSTONE/probak/kartak/KartaProbak.java
+++ 1.HEARTHSTONE/probak/kartak/KartaProbak.java
@@ -6,19 +6,35 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import trebetasunak.Trebetasuna;
+
 public class KartaProbak {
+	Morroia mo1, mo2, mo3, mo4;
+	Trebetasuna  erasoJarraia, diana;
 
 	@Before
 	public void setUp() throws Exception {
+		// int pIdKarta, String pIzena, String pDeskribapena, int pBalioa, boolean pErasoAhalDu
+		mo1= new Morroia (0001, "Otso Iluna", "Trebetasunik gabeko morroia, baina oso eraginkorra", 2, 2, 3, null);
+		mo2 = new Morroia (0002, "Txerri Humoretsua", "Eraso jarraia duen morroia haserrea, jarraieran eraso dezake", 3, 3, 1, null );
+//		mo3 = new Morroia ();
+		// 0002-Txerri Humoretsua-Eraso Jarraia duen morroi haserrea, jarraieran eraso dezake-3-3-1-Eraso Jarraia;
+		//int pIdKarta, String pIzena, String pDeskribapena, int pBalioa, int pErasoa, int pBizitza, Trebetasuna pTrebetasuna
+		
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		mo1 = null;
+		mo2 = null;
 	}
 
 	@Test
 	public void testKarta() {
-		fail("Not yet implemented");
+		assertNotNull(mo1);
+		assertNotNull(mo2);
+//		assertNotNull(mo3);
+//		assertNotNull(mo4);
 	}
 
 	@Test
